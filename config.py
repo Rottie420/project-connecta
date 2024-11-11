1553ed87ed012929280164c2e98268d6178989d8
Rottie420 <93668355+Rottie420@users.noreply.github.com>
2024-11-11 12:17:19 +0800

    revert static/uploads

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 JSON_FILE_PATH = 'pets.json'
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 LOG_FILE_PATH = 'log.txt'
 DATA_FILE = 'consultations.json'
