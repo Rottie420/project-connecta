1ab9f5c1cd8ffa2152a621c75b0619cee38e7c62
Rottie420 <93668355+Rottie420@users.noreply.github.com>
2024-11-26 14:15:44 +0800

    with open

--- FileHandler.py
+++ FileHandler.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 from PIL import Image
 from werkzeug.utils import secure_filename
 from config import UPLOAD_FOLDER, ALLOWED_EXTENSIONS
@@ -14,21 +13,20 @@ class FileHandler:
     def save_and_convert_image(file, control_number):
         filename = secure_filename(file.filename)
         unique_filename = f"{control_number}_{filename}"
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            original_path = temp_file.name
-            file.save(original_path)
-
-            webp_filename = f"{control_number}.webp"
-            webp_path = os.path.join(UPLOAD_FOLDER, webp_filename)
-            
-            try:
-                with Image.open(original_path) as img:
-                    img.save(webp_path, 'webp')
-            except Exception as e:
-                Logger.log(f"Image conversion error for {filename} ({original_path}): {e}")
-                raise
-            finally:
-                os.remove(original_path)  # Clean up temporary file
-            
-        return f"{UPLOAD_FOLDER}/{webp_filename}"
+        original_path = os.path.join(UPLOAD_FOLDER, unique_filename)
         
+        file.save(original_path)
+        
+        # Convert to WEBP format
+        webp_filename = f"{control_number}.webp"
+        webp_path = os.path.join(UPLOAD_FOLDER, webp_filename)
+        
+        try:
+            with Image.open(original_path) as img:
+            img.save(webp_path, 'webp')
+        except Exception as e:
+            Logger.log(f"Image conversion error for {filename}: {e}")
+            raise
+        finally:
+            os.remove(original_path)  # Clean up original file
+        return f"{UPLOAD_FOLDER}/{webp_filename}"      
